13dbb885cec33584e02c54110d5e96ab09b09bda
Fernando Lima <pardalismitis@gmail.com>
2022-03-30 12:47:04 -0300

    criação e ajustes

---
+++ ajustesTaxonomia.R
@@ -0,0 +1,73 @@
+rm(list = ls(all = TRUE))
+#install.packages("here")
+#install.packages("tidyverse")
+#install.packages("ggplot2")
+#install.packages("vctrs")
+#library(tidyverse)
+library(xlsx)
+#library(ggplot2)
+library(here)
+library(stringr)
+#library(vctrs)
+
+mamiferos = 
+  read.csv("occ_v_sp.csv",
+           #sep = ";",
+           na.strings = c("", "NA"),
+           encoding = "UTF-8"
+  )
+
+mamiferos$genus <- word(mamiferos$species,1)
+
+familia <- subset(mamiferos$genus, is.na(mamiferos$family))
+
+familiaLista <- unique(familia)
+
+AccChars <- "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ "
+RegChars <- "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy_"
+
+for(i in 1:ncol(mapeamento)) {       
+  mapeamento[ , i] <- chartr(AccChars, RegChars, mapeamento[ , i])
+  mapeamento[ , i] <- tolower(mapeamento[ , i])
+}
+
+
+
+
+AccChars <- "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ "
+RegChars <- "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy_"
+
+for(i in 1:ncol(mapeamento)) {       
+  mapeamento[ , i] <- chartr(AccChars, RegChars, mapeamento[ , i])
+  mapeamento[ , i] <- tolower(mapeamento[ , i])
+}
+
+
+mapeamento$X.U.FEFF.UC <- gsub("rio_cautario","Rio Cautário",
+                               gsub("ouro_preto","Rio Ouro Preto",
+                                    gsub("cazumba","Cazumbá-Iracema",
+                                         mapeamento$X.U.FEFF.UC)))
+
+
+mapeamento$cap <- as.integer(mapeamento$cap)
+mapeamento$cap[is.na(mapeamento$cap)] <- 0
+
+for(i in 1:nrow(mamiferos)){
+  if (mamiferos$family[!is.na(mamiferos$family)]) {
+    mamiferos$family <- mamiferos$family
+  } else if (mamiferos$genus == "Alouatta" | "Brachyteles"){
+    family <- "Atelidae"
+  #} #else if (mapeamento$cap[i] > 50 & mapeamento$cap[i] <= 100){
+    #tamanho <- "jovem-adulto"
+  #} #else if (mapeamento$cap[i] > 100 & mapeamento$cap[i] <= 150){
+   # tamanho <- "adulto"
+  #} else if (mapeamento$cap[i] > 150 & mapeamento$cap[i] <= 200){
+   # tamanho <- "adulto-senescente"
+  } else {
+     family <- ""
+  }
+  mamiferos$family[i] <- family
+}
+
+
+write.xlsx(mapeamento, here("output", "formulario6Mapeamento.xlsx"), sheetName = "formulario6Mapeamento", col.names = TRUE, row.names = TRUE, append = FALSE)
